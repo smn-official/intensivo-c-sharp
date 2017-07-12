4f6c4924677b3d3536613249eee7a38c98b68c36
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2017-07-12 18:16:42 -0300

    Update Popular Base.sql
    Adicionado execução do Update na tabela de usuário para criptografar as senhas.

--- DB/Popular Base.sql
+++ DB/Popular Base.sql
@@ -1,11 +1,16 @@
 INSERT INTO [dbo].[Acao] (Nome)
-	VALUES ('Recebido'), ('Pago'), ('Lançamento Futuro'), ('Aberto');
+	VALUES ('Recebido'), ('Pago'), ('LanÃ§amento Futuro'), ('Aberto');
 
 INSERT INTO [dbo].[Categoria] (Nome)
-	VALUES ('Lazer'), ('Serviços'), ('Eletrônicos'), ('Outros');
+	VALUES ('Lazer'), ('ServiÃ§os'), ('EletrÃ´nicos'), ('Outros');
 
 INSERT INTO [dbo].[Usuario] (Email, Senha, FG_Ativo)
 	VALUES ('lucas.felix@smn.com.br', '123456', 1), ('zezinho@gmail.com', '24445', 1), ('maria@hotmail.com', '998776', 0);
 
 INSERT INTO [dbo].[Conta] (Nome, DataCadastro, IdUsuario)
-	VALUES ('Itaú', GETDATE(), 1), ('Conta 00', GETDATE(), 2), ('Minhas Finanças', GETDATE(), 3);
+	VALUES ('ItaÃº', GETDATE(), 1), ('Conta 00', GETDATE(), 2), ('Minhas FinanÃ§as', GETDATE(), 3);
+	
+-- NÃ£o queremos senhas descriptografadas no banco!
+EXEC [dbo].[GKSSP_UpdUsuario] 1, 'lucas.felix@smn.com.br', '123456';
+EXEC [dbo].[GKSSP_UpdUsuario] 2, 'zezinho@gmail.com', '24445';
+EXEC [dbo].[GKSSP_UpdUsuario] 3, 'maria@hotmail.com', '998776';
