2b76dbc5af8ddea68226ec19e44747beb915c444
DESKTOP-2SK5IJG\Guilherme Sousa <guilhermefsousa23@gmail.com>
2017-07-14 01:27:24 -0300

    Demo - Encapsulamento POO

---
+++ Programação Orientada a Objetos/Demos/Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número de 1 a 9: ");
+            var resposta = int.Parse(Console.ReadLine());
+
+            Som.EmitirBeep(resposta); //Emitindo um som, não sabemos como, mas sabemos que é só chamar e passar o tanto de vezes que queremos o beep
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programação Orientada a Objetos/Demos/Encapsulamento/Encapsulamento/Som.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    public class Som
+    {
+        public static void EmitirBeep(int quantidade)
+        {
+            if (quantidade > 0 && quantidade < 10)
+            {
+                for (int i = 1; i <= quantidade; i++)
+                {
+                    Console.WriteLine("Beep número {0}.", i);
+                    Console.Beep();
+                }
+            }
+            else
+                Console.WriteLine("Entre com um numero de 1 a 9");
+        }
+
+    }
+}
