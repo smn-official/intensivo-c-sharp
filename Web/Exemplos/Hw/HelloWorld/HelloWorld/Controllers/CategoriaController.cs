e5417b34bd3286c8cab4cf7fda4ad5aca7ebbe90
Bruno Barbosa <bruno.barbosa@engsolutions.com.br>
2017-07-25 17:04:23 -0300

    Adicionando projeto de exemplo - MVC Vespertino

---
+++ Web/Exemplos/Hw/HelloWorld/HelloWorld/Controllers/CategoriaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BuscarGrid()
+        {
+            return PartialView("_Grid");
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View("_Dados");
+        }
+    }
+}
---
+++ Web/Exemplos/Hw/HelloWorld/HelloWorld/Controllers/LancamentoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class LancamentoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BuscarGrid()
+        {
+            return View("_Grid");
+        }
+    }
+}
